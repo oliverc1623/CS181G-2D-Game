451eabee6d2de66f925eb2a98c79b8e5b3cc728a
Oliver Chang <ollieskywalker@gmail.com>
2021-04-02 15:03:35 -0700

    networking works in overworld!

--- src/states.rs
+++ src/states.rs
@@ -115,7 +115,7 @@ impl State for Title {
         // _game.players.get_mut(&_game.server.id).unwrap().pos.0 = _game.players.get_mut(&_game.server.id).unwrap().vel.0;
         // _game.players.get_mut(&_game.server.id).unwrap().pos.1 = _game.players.get_mut(&_game.server.id).unwrap().vel.1;
 
-        _game.server.update_game(&mut _game.players);
+        _game.server.update_players(&mut _game.players);
 
         // for posn in _game.other_players.iter_mut() {
         //     posn.0 += vel.0;
@@ -129,6 +129,7 @@ impl State for Title {
 
         if key_input.key_held(VirtualKeyCode::P) {
             // println!("hitting p");
+            _game.players.get_mut(&_game.server.id).unwrap().world = 1;
             StateResult::Swap(Box::new(Scroll()))
         } else {
             StateResult::Keep
@@ -153,16 +154,16 @@ impl State for Title {
         // draw main player        
         let curpos = _game.players[&_game.server.id].pos;
         // println!("player pos {:?}", &_game.textures[1].image);
-        screen.bitblt(&_game.textures[0], _game.anim_state[0].frame(), curpos);
+        // screen.bitblt(&_game.textures[0], _game.anim_state[0].frame(), curpos);
         // draw positions of other players
-        // for ((player, tex), anim) in _game
-        //     .players
-        //     .iter()
-        //     .zip(_game.textures.iter())
-        //     .zip(_game.anim_state.iter())
-        // {
-        //     screen.bitblt(tex, anim.frame(), player.1.pos);
-        // }
+        for ((player, tex), anim) in _game
+            .players
+            .iter()
+            .zip(_game.textures.iter())
+            .zip(_game.anim_state.iter())
+        {
+            screen.bitblt(tex, anim.frame(), player.1.pos);
+        }
     }
 }
 #[derive(Debug)]
@@ -177,6 +178,7 @@ impl State for Scroll {
         key_input: &WinitInputHelper,
     ) -> StateResult {
         _game.level = 0;
+        let cur_player = _game.players.get_mut(&_game.server.id).unwrap();
         // StateResult::Keep
         let max_vel = 20;
         let min_vel = -20;
@@ -184,47 +186,46 @@ impl State for Scroll {
         if key_input.key_held(VirtualKeyCode::Right){
             // _game.velocities[0].0 = 7;
             // println!("right vel {:}", _game.velocities[0].0);
-            if _game.players.get_mut(&_game.server.id).unwrap().vel.0 < max_vel {
+            if cur_player.vel.0 < max_vel {
                 // println!("inside");
-                _game.players.get_mut(&_game.server.id).unwrap().vel.0 +=2;
+                cur_player.vel.0 +=2;
                 // println!("inside {:}", _game.velocities[0].0);
             }
             _game.anim_state[0].tick();
         } else if key_input.key_released(VirtualKeyCode::Right) {
-            _game.players.get_mut(&_game.server.id).unwrap().vel.0 = (_game.players.get_mut(&_game.server.id).unwrap().vel.0 as f32 * 0.25) as i32;
+            cur_player.vel.0 = (cur_player.vel.0 as f32 * 0.25) as i32;
             // println!("after {:}", _game.velocities[0].0);
         }
         if key_input.key_held(VirtualKeyCode::Left) {
-            if _game.players.get_mut(&_game.server.id).unwrap().vel.0 > min_vel {
+            if cur_player.vel.0 > min_vel {
                 // println!("inside");
-                _game.players.get_mut(&_game.server.id).unwrap().vel.0 -=2;
+                cur_player.vel.0 -=2;
                 // println!("inside {:}", _game.velocities[0].0);
             }
         } else if key_input.key_released(VirtualKeyCode::Left) {
-            _game.players.get_mut(&_game.server.id).unwrap().vel.0 = (_game.players.get_mut(&_game.server.id).unwrap().vel.0 as f32 * 0.25) as i32;
+            cur_player.vel.0 = (cur_player.vel.0 as f32 * 0.25) as i32;
         }
         if key_input.key_held(VirtualKeyCode::Up) {
             if _game.game_data.num_jumps < 2 {
-                _game.players.get_mut(&_game.server.id).unwrap().vel.1 = -5;            
+                cur_player.vel.1 = -5;            
                 _game.game_data.num_jumps += 1;
             }
         } else if key_input.key_released(VirtualKeyCode::Up) {
-            _game.players.get_mut(&_game.server.id).unwrap().vel.1 /= 2;
-            _game.players.get_mut(&_game.server.id).unwrap().vel.1 = 2;            
+            cur_player.vel.1 /= 2;
+            cur_player.vel.1 = 2;            
         }
         if key_input.key_held(VirtualKeyCode::Down) {
-            if _game.players.get_mut(&_game.server.id).unwrap().vel.1 < max_vel {
-                _game.players.get_mut(&_game.server.id).unwrap().vel.1 +=2;
+            if cur_player.vel.1 < max_vel {
+                cur_player.vel.1 +=2;
                 // println!("inside {:}", _game.velocities[0].0);
             }
         } else if key_input.key_released(VirtualKeyCode::Down) {
-            _game.players.get_mut(&_game.server.id).unwrap().vel.1 = (_game.players.get_mut(&_game.server.id).unwrap().vel.1 as f32 * 0.25) as i32;
+            cur_player.vel.1 = (cur_player.vel.1 as f32 * 0.25) as i32;
         }
         // Update all entities' positions
-
         // update current player
-        _game.players.get_mut(&_game.server.id).unwrap().pos.0 = _game.players.get_mut(&_game.server.id).unwrap().vel.0;
-        _game.players.get_mut(&_game.server.id).unwrap().pos.1 = _game.players.get_mut(&_game.server.id).unwrap().vel.1;
+        cur_player.pos.0 = cur_player.vel.0;
+        cur_player.pos.1 = cur_player.vel.1;
 
         // for (posn, vel) in _game.positions.iter_mut().zip(_game.velocities.iter()) {
         //     posn.0 += vel.0;
@@ -251,7 +252,7 @@ impl State for Scroll {
             &[&levels[_game.level].0[0]],
             &mut contacts,
         );
-
+        _game.server.update_players(&mut _game.players);
         // update camera after restitution
         _game.camera.0 += _game.players[&_game.server.id].vel.0;
         _game.background_pos.0 += -1*_game.players[&_game.server.id].vel.0;
@@ -260,7 +261,9 @@ impl State for Scroll {
         // _game.camera.1 += _game.velocities[0].1;
 
         if key_input.key_held(VirtualKeyCode::X) {
-            StateResult::Remove
+            // StateResult::Remove
+            _game.players.get_mut(&_game.server.id).unwrap().world = 0;
+            StateResult::Swap(Box::new(Title()))
         } else {
             StateResult::Keep
         }
